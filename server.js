5eb8d696fdf9dee32165b8d2ccca2c337293dfd3
justinkunz <justin_kunz@yahoo.com>
2019-11-12 20:54:58 -0600

    fixed port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
@@ -15,4 +16,4 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mernDb");
-app.listen(3000);
+app.listen(PORT);
